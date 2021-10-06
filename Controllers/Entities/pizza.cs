85922b7c9ef53eba71133cf7b3a80129884d48f9
SamuelJacome <samuelvictor@rede.ulbra.br>
2021-10-06 08:56:42 -0300

    first commit

---
+++ Controllers/Entities/pizza.cs
@@ -0,0 +1,9 @@
+namespace ContosoPizza.Controllers.Entities
+{
+    public class pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
